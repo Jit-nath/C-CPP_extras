82343f2180279d8a09be67fd41178691e4832aa0
Jit Debnath <jit.nathdeb@gmail.com>
2024-10-07 16:38:41 +0530

    commit

--- CPP/practice.cpp
+++ CPP/practice.cpp
@@ -3,21 +3,6 @@ using namespace std;
 
 int main()
 {
-    char *p = new char[100];
-
-    char *t = p;
-
-    *p = 'a';
-    p++;
-    *p = 'b';
-    p++;
-    *p = '\0';
-    while (*t != '\0')
-    {
-        cout << *t;
-        t++;
-    }
-
-    delete[] p;
+    cout<<"working perfectly";
     return 0;
 }
