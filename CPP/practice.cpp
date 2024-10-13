b4bc740ca479e281a2019a4aaf396194bf80f4b7
Jit Debnath <jit.nathdeb@gmail.com>
2024-10-13 09:44:50 +0000

    huu

--- CPP/practice.cpp
+++ CPP/practice.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
-
-
+void print(const char* string){
+    cout << string ;
+}
+void println(const char* string){
+    cout<< string << endl;
+}
 int main()
 {
-    cout<<"working perfectly";
+    println("hello world with newline");
+    print("hello world");
     return 0;
 }
+
