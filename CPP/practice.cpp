6ad704f98c55b9659df47ccce9ae790af1765b2d
Jit Debnath <jit.nathdeb@gmail.com>
2024-10-26 18:35:34 +0530

    vnbmv

--- CPP/practice.cpp
+++ CPP/practice.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void print(const char* string){
-    cout << string ;
-}
-void println(const char* string){
-    cout<< string << endl;
-}
-int main()
+void print(const char *string)
 {
-    println("hello world with newline");
-    print("hello world");
-    return 0;
+    cout << string;
 }
-
+void println(const char *string)
+{
+    cout << string << endl;
+}
